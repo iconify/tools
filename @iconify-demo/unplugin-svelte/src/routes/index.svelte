f965b649fd3bd33364b20ad8bd78250c58cc6234
Vjacheslav Trushkin <cyberalien@gmail.com>
2022-04-04 18:09:05 +0300

    Import an entire icon set in unplugin-icons demo

--- @iconify-demo/unplugin-svelte/src/routes/index.svelte
+++ @iconify-demo/unplugin-svelte/src/routes/index.svelte
@@ -3,14 +3,25 @@
     import SVGIonAccessibility from 'virtual:icons/svg-ion/accessibility'
     import SVGIonBalloonOutline from 'virtual:icons/svg-ion/balloon-outline'
 
+    // Import as directory
+    import SVGAnimatedLoading from 'virtual:icons/svg-animated/loading';
+    import SVGAnimatedLoading2 from 'virtual:icons/svg-animated/loading2';
+
 </script>
 
 <main>
     <section>
-        <h1>Importing from 512x512 monotone SVG</h1>
+        <h1>Importing from 512x512 monotone SVG (imported one by one on demand)</h1>
         <p>Black 1em icon, changes color on hover: <SVGIonAccessibility /></p>
-        <p>Green 2em icon: <SVGIonBalloonOutline style="font-size: 2em; color: #327335;"/></p>
-        
+        <p>Green 2em icon: <SVGIonBalloonOutline style="font-size: 2em; color: #327335;"/></p>    
+    </section>
+
+    <section>
+        <h1>Importing from 24x24 animated monotone SVG (imported as icon set from directory)</h1>
+        <p class="animated24">
+            <SVGAnimatedLoading />
+            <SVGAnimatedLoading2 />
+        </p>
     </section>
 </main>
 
@@ -29,7 +40,7 @@
         transition: color 0.2s ease;
     }
     section:hover p {
-        color: #026c9c;
+        color: #ba3329;
     }
 
     section h1 {
@@ -37,6 +48,12 @@
         line-height: 32px;
         margin: 0 0 16px;
         padding: 0;
+    }
+
+    .animated24 {
+        font-size: 24px;
+        line-height: 1em;
+        color: #026c9c;
     }
 
     p {
--- @iconify-demo/unplugin-svelte/svelte.config.js
+++ @iconify-demo/unplugin-svelte/svelte.config.js
@@ -6,6 +6,7 @@ import {
 	parseColors,
 	runSVGO,
 	deOptimisePaths,
+	importDirectory,
 } from '@iconify/tools';
 import preprocess from 'svelte-preprocess';
 import Icons from 'unplugin-icons/vite';
@@ -23,6 +24,62 @@ const config = {
 				Icons({
 					compiler: 'svelte',
 					customCollections: {
+						// Load an entire icon set
+						'svg-animated': async () => {
+							// Load icons
+							const iconSet = await importDirectory(
+								assetsRootDir + '/svg-animated',
+								{
+									prefix: 'svg-animated',
+								}
+							);
+
+							// Clean up each icon
+							await iconSet.forEach(async (name) => {
+								const svg = iconSet.toSVG(name);
+
+								// Change color to `currentColor`
+								const blackColor = stringToColor('black');
+
+								await parseColors(svg, {
+									defaultColor: 'currentColor',
+									callback: (attr, colorStr, color) => {
+										// console.log('Color:', colorStr, color);
+
+										// Change black to 'currentColor'
+										if (
+											color &&
+											compareColors(color, blackColor)
+										) {
+											return 'currentColor';
+										}
+
+										switch (color?.type) {
+											case 'none':
+											case 'current':
+												return color;
+										}
+
+										throw new Error(
+											`Unexpected color "${colorStr}" in attribute ${attr}`
+										);
+									},
+								});
+
+								// Optimise, but do not change shapes because they are animated
+								await runSVGO(svg, {
+									keepShapes: true,
+								});
+
+								// Update icon in icon set
+								iconSet.fromSVG(name, svg);
+							});
+
+							// Export as IconifyJSON
+							return iconSet.export();
+						},
+
+						// Load icon one by one on demand
 						'svg-ion': async (name) => {
 							// Load icon
 							const filename = `${assetsRootDir}/svg-ion/${name}.svg`;
