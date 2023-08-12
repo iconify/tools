6a2d8398fb0c2a8f88e7383713c1ab5256ca4c1b
Vjacheslav Trushkin <cyberalien@gmail.com>
2023-08-12 17:20:07 +0300

    chore: update unocss demo

--- @iconify-demo/unocss/unocss.config.ts
+++ @iconify-demo/unocss/unocss.config.ts
@@ -1,65 +1,73 @@
-import { promises as fs } from 'fs';
-import { defineConfig, presetUno } from 'unocss';
-import type { IconifyJSON } from '@iconify/types';
-import presetIcons from '@unocss/preset-icons';
+import { promises as fs } from 'node:fs';
+import { defineConfig, presetIcons, presetUno } from 'unocss';
 import { compareColors, stringToColor } from '@iconify/utils/lib/colors';
+import type { IconSet } from '@iconify/tools';
 import {
+	deOptimisePaths,
 	importDirectory,
 	parseColors,
 	runSVGO,
-	deOptimisePaths,
 } from '@iconify/tools';
+import type { CustomIconLoader } from '@iconify/utils/lib/loader/types';
 
 /**
  * Load custom icon set
  */
-async function loadCustomIconSet(): Promise<IconifyJSON> {
-	// Load icons
-	const iconSet = await importDirectory('assets/svg', {
-		prefix: 'svg',
-	});
+function loadCustomIconSet(): CustomIconLoader {
+	const promise = new Promise<IconSet>((resolve, reject) => {
+		importDirectory('assets/svg', {
+			prefix: 'svg',
+		}).then((iconSet) => {
+			iconSet
+				.forEach(async (name) => {
+					const svg = iconSet.toSVG(name)!;
 
-	// Clean up each icon
-	await iconSet.forEach(async (name) => {
-		const svg = iconSet.toSVG(name)!;
+					// Change color to `currentColor`
+					const blackColor = stringToColor('black')!;
 
-		// Change color to `currentColor`
-		const blackColor = stringToColor('black')!;
+					await parseColors(svg, {
+						defaultColor: 'currentColor',
+						callback: (attr, colorStr, color) => {
+							// console.log('Color:', colorStr, color);
 
-		await parseColors(svg, {
-			defaultColor: 'currentColor',
-			callback: (attr, colorStr, color) => {
-				// console.log('Color:', colorStr, color);
+							// Change black to 'currentColor'
+							if (color && compareColors(color, blackColor))
+								return 'currentColor';
 
-				// Change black to 'currentColor'
-				if (color && compareColors(color, blackColor)) {
-					return 'currentColor';
-				}
+							switch (color?.type) {
+								case 'none':
+								case 'current':
+									return color;
+							}
 
-				switch (color?.type) {
-					case 'none':
-					case 'current':
-						return color;
-				}
+							throw new Error(
+								`Unexpected color "${colorStr}" in attribute ${attr}`
+							);
+						},
+					});
 
-				throw new Error(
-					`Unexpected color "${colorStr}" in attribute ${attr}`
-				);
-			},
+					// Optimise
+					runSVGO(svg);
+
+					// Update paths for compatibility with old software
+					await deOptimisePaths(svg);
+
+					// Update icon in icon set
+					iconSet.fromSVG(name, svg);
+				})
+				.then(() => {
+					resolve(iconSet);
+				})
+				.catch((err) => {
+					reject(err);
+				});
 		});
-
-		// Optimise
-		runSVGO(svg);
-
-		// Update paths for compatibility with old software
-		await deOptimisePaths(svg);
-
-		// Update icon in icon set
-		iconSet.fromSVG(name, svg);
 	});
 
-	// Export as IconifyJSON
-	return iconSet.export();
+	return async (name) => {
+		const iconSet = await promise;
+		return iconSet.toSVG(name)?.toMinifiedString();
+	};
 }
 
 /**
