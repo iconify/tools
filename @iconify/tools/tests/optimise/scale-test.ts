0c09974320a650d9fc025ac53f12d5a2b1f300a5
Vjacheslav Trushkin <cyberalien@gmail.com>
2023-11-28 10:33:28 +0200

    chore: use SVGO 3.0.3

--- @iconify/tools/tests/optimise/scale-test.ts
+++ @iconify/tools/tests/optimise/scale-test.ts
@@ -25,7 +25,7 @@ describe('Scaling icon', () => {
 
 		scaleSVG(svg, 1 / 40);
 		expect(svg.toMinifiedString()).toBe(
-			'<svg width="24" height="24" viewBox="0 0 24 24"><path d="M12 15h1.5v-2.25L15.25 15h1.825l-2.325-3 2.325-3H15.25l-1.75 2.25V9H12v6Zm-3.5 0H10V9H7v1.5h1.5V15ZM5 21q-.825 0-1.413-.588T3 19V5q0-.825.588-1.413T5 3h14q.825 0 1.413.588T21 5v14q0 .825-.588 1.413T19 21H5Zm0-2h14V5H5v14ZM5 5v14V5Z"/></svg>'
+			'<svg width="24" height="24" viewBox="0 0 24 24"><path d="M12 15h1.5v-2.25L15.25 15h1.825l-2.325-3 2.325-3H15.25l-1.75 2.25V9H12v6Zm-3.5 0H10V9H7v1.5h1.5V15ZM5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h14q.825 0 1.413.588T21 5v14q0 .825-.587 1.413T19 21H5Zm0-2h14V5H5v14ZM5 5v14V5Z"/></svg>'
 		);
 		expect(svg.viewBox).toEqual({
 			left: 0,
