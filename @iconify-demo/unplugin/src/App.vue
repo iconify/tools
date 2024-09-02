41eae4591bf41b37546f9c1b12da22fa4e0ce0be
Vjacheslav Trushkin <cyberalien@gmail.com>
2024-09-02 09:32:21 +0300

    chore: add unplugin demo

---
+++ @iconify-demo/unplugin/src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import LoadingIcon from '~icons/svg-animated/loading';
+import LoadingIcon2 from '~icons/svg-animated/loading2';
+import BuildIcon from '~icons/svg-ion/build';
+import AccessibilityIcon from '~icons/svg-ion/accessibility';
+</script>
+<template>
+	<main>
+		<h1>Unplugin Icons demo</h1>
+		<p>
+			Custom icons with `svg-ion` prefix:
+			<BuildIcon /><AccessibilityIcon />
+		</p>
+		<p>Custom animated icons: <LoadingIcon /><LoadingIcon2 /></p>
+		<p>Icons use text color, hover icon to see color change</p>
+	</main>
+</template>
+
+<style>
+main {
+	font-size: 16px;
+	line-height: 1.5;
+}
+h1 {
+	font-size: 22px;
+}
+p {
+	font: inherit;
+}
+p svg {
+	vertical-align: -0.25em;
+}
+p:hover {
+	color: rgb(8, 98, 172);
+}
+p svg:hover {
+	color: rgb(211, 15, 15);
+}
+</style>
