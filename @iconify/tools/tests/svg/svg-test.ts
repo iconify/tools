8ebc0fbc150f31fc704669d1f059b1c9483e8d2f
Vjacheslav Trushkin <cyberalien@gmail.com>
2025-12-29 17:44:19 +0200

    chore: test SVG toPrettyString method

--- @iconify/tools/tests/svg/svg-test.ts
+++ @iconify/tools/tests/svg/svg-test.ts
@@ -20,6 +20,9 @@ describe('Loading SVG', () => {
 		expect(svg.toMinifiedString()).toBe(
 			'<svg xmlns="http://www.w3.org/2000/svg" width="12" height="20" viewBox="-8 -16 24 40"><path d="M3 0v1h4v5h-4v1h5v-7h-5zm1 2v1h-4v1h4v1l2-1.5-2-1.5z"/></svg>'
 		);
+		expect(svg.toPrettyString()).toBe(
+			'<svg xmlns="http://www.w3.org/2000/svg" width="12" height="20" viewBox="-8 -16 24 40">\n\t<path d="M3 0v1h4v5h-4v1h5v-7h-5zm1 2v1h-4v1h4v1l2-1.5-2-1.5z" />\n</svg>\n'
+		);
 		expect(svg.getBody()).toBe(
 			'<path d="M3 0v1h4v5h-4v1h5v-7h-5zm1 2v1h-4v1h4v1l2-1.5-2-1.5z"/>'
 		);
